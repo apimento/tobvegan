438bcc0f8cccd087ba03957a086a3e65453db6ac
apimento <98665437+apimento@users.noreply.github.com>
2022-03-28 18:28:55 -0400

    added style to model

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Restaurant(models.Model): 
     name = models.CharField(max_length=100) 
     pricePoint= models.CharField (max_length=6)  
-    contact=models.CharField(max_length=200) 
+    contact=models.CharField(max_length=200)
+    style=models.CharField(max_length=100) 
     menuHighlights = models.CharField(max_length=200) 
     delivery= models.BooleanField()
     events = models.BooleanField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from .models import Restaurant
 
 # Create your views here.
 
-class Restaurant: 
-    def __init__(self, name, contact, pricePoint, style, menuHighlights, delivery, events):
-        self.name = name 
-        self.contact = contact 
-        self.pricePoint = pricePoint  
-        self.style = style
-        self.menuHighlights = menuHighlights  
-        self.delivery = delivery 
-        self.events = events
+# class Restaurant: 
+#     def __init__(self, name, contact, pricePoint, style, menuHighlights, delivery, events):
+#         self.name = name 
+#         self.contact = contact 
+#         self.pricePoint = pricePoint  
+#         self.style = style
+#         self.menuHighlights = menuHighlights  
+#         self.delivery = delivery 
+#         self.events = events
 
-restaurants = [ 
-]
+# restaurants = [ 
+# ]
 
 def home(request): 
     return HttpResponse('<h1> T.O b Vegan </h1>') 
@@ -24,6 +25,7 @@ def about(request):
     return render(request,'about.html') 
 
 def restaurants_index(request): 
+    restaurants =  Restaurant.objects.all()
     return render(request, 'restaurants/index.html', {'restaurants': restaurants}) 
 
 def restaurants_details(request, restaurant_id):
