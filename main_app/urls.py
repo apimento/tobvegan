0a7f73c6574537301bb2a925fff9ed232eeeabcd
apimento <98665437+apimento@users.noreply.github.com>
2022-03-30 21:16:09 -0400

    actual EOD

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('restaurants/', views.restaurants_index, name="index"),
     path("restaurants/<int:restaurant_id>/", views.restaurants_details, name="detail"),
+    path("restaurants/create", views.RestaurantCreate.as_view(), name='restaurant_create'),
     path('restaurants/<int:pk>/update/', views.RestaurantUpdate.as_view(), name="restaurant_update"),
     path('restaurants/<int:pk>/delete/', views.RestaurantDelete.as_view(), name="restaurant_delete"),
     path('restaurants/<int:restaurant_id>/add_menuHighlight', views.add_menuHighlight, name="add_menuHighlight"),   
-    path('restaurants/<int:restaurant_id>/delete_menuHighlight', views.delete_Menuhighlight, name="delete_menuhighlight") 
+    path('restaurants/<int:restaurant_id>', views.delete_Menuhighlight, name="delete_menuhighlight") 
 
 ]
